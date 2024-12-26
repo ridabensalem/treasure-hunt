0a08067de1cb4190f4bab50a7a7017287fae0214
bensalem <rida.bensalem@sunrise.net>
2024-12-26 14:23:16 +0100

    create the endpoint of the game

--- app.js
+++ app.js
@@ -1,4 +1,45 @@
-// intialise express 
-const express=require("express");
-const app=express(); 
-app.use(express.json()); 
+const express = require('express');
+const app = express();
+
+// Sample data
+let areas = [
+    { id: 1, name: 'Golden Forest', treasure: 50 },
+    { id: 2, name: 'Crystal Cave', treasure: 100 },
+    { id: 3, name: 'Sunken Ship', treasure: 75 },
+];
+
+let totalTreasureCollected = 0;
+
+// Get available areas
+app.get('/api/areas', (req, res) => {
+    res.status(200).send(areas);
+});
+
+// Collect treasure from an area
+app.post('/api/areas/:id/collect', (req, res) => {
+    const areaId = parseInt(req.params.id);
+    const area = areas.find(a => a.id === areaId);
+
+    if (!area) {
+        return res.status(404).send({ error: 'Oh No Traverler you are lost !' });
+    }
+
+    if (area.treasure === 0) {
+        return res.status(400).send({ error: 'You are too late ,there is no treasure here ' });
+    }
+
+    totalTreasureCollected += area.treasure;
+    area.treasure = 0;
+
+    res.status(200).json({
+        message: `You collected treasure from ${area.name}!`,
+        totalTreasureCollected,
+    });
+
+   
+});
+app.listen(3000, () => {
+    console.log('Server running on http://localhost:3000');
+});
+
+module.exports = app;
